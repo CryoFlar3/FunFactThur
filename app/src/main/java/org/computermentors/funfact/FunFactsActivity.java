7fd5f0dc750495292422f23440ba82ea0987a5d1
CryoFlar3 <ndukes@computermentors.org>
2018-08-30 19:28:25 -0400

    Initial commit

---
+++ app/src/main/java/org/computermentors/funfact/FunFactsActivity.java
@@ -0,0 +1,32 @@
+package org.computermentors.funfact;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class FunFactsActivity extends AppCompatActivity {
+
+    private TextView factTextView;
+    private Button showFactButton;
+    private RelativeLayout relativeLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fun_facts);
+
+        factTextView = findViewById(R.id.factTextView);
+        showFactButton = findViewById(R.id.showFactButton);
+        relativeLayout = findViewById(R.id.funFactLayout);
+
+        showFactButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
